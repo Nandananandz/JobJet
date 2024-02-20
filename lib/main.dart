86264b0b095d86c20c20943755fae8d3d3383728
unknown <nandanasanal321@gmail.com>
2024-02-20 11:59:32 +0530

    tel

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jobjet/dashboard.dart';
 import 'package:sizer/sizer.dart';
-
+ double width = 4.14;
+ double height = 8.9;
+ double fontsize = 1.2;
 void main() {
   runApp(JobJet());
 }
