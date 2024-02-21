473cf94d124174c58780cd174b9cf60d09c66a37
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-02-21 17:28:08 +0530

    fix

--- lib/Screens/UserProfile/components/UserDetailCard.dart
+++ lib/Screens/UserProfile/components/UserDetailCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 
 class UserDetailScreen extends StatelessWidget {
@@ -9,76 +10,80 @@ class UserDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading:Container(
-        margin: EdgeInsets.all(8),
+        margin: EdgeInsets.only(left: 3.86.w,bottom: 1.46.h,top: 1.23.h,right: 1.46.w),
         alignment: Alignment.center,
         height: 4.04.h,
         width: 8.69.w,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
+          borderRadius: BorderRadius.circular(5),
           color: Color(0xFFE8ECFF)
         ),
         child: InkWell(
           onTap: () { 
             Navigator.pop(context);
            },
-          child: Icon(Icons.chevron_left_sharp,color: Color(0xFF121B54),),
+          child: Icon(Icons.arrow_back_ios_new_rounded,size: 1.8.h,color: Color(0xFF121B54),),
         ),
-      )),
+        )
+        ),
 
 
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 1.23.h),
-            Text('Complete your profile'),
-            SizedBox(height: 3.93.h),
-
-            Text('User name'),
-            SizedBox(height: 0.89.h),
-            textfield(),
-            SizedBox(height: 1.68.h,),
-
-
-            Text('Job type'),
-            SizedBox(height: 0.89.h),
-            textbox(),
-            SizedBox(height: 1.68.h,),
-
-            Text('Total years of experience'),
-            SizedBox(height: 0.89.h),
-            textbox(),
-            SizedBox(height: 1.68.h,),
-
-            Text('Job category'),
-            SizedBox(height: 0.89.h),
-            textbox(),
-            SizedBox(height: 1.68.h,),
-
-            Text('Current job time'),
-            SizedBox(height: 0.89.h),
-            textbox(),
-            SizedBox(height: 1.68.h,),
-
-            Text('Resume link'),
-            SizedBox(height: 0.89.h),
-            textfield(),
-            SizedBox(height: 6.17.h,),
-
-            Container(
-              alignment: Alignment.center,
-              height: 5.50.h,
-              width: 85.40.w,
-              decoration: BoxDecoration(
-                color: Color(0xFF1F41BA),
-                borderRadius:BorderRadius.circular(30),
-              ),
-              child: Text('Submit your profile',
-              style:TextStyle(color: Color(0xFFFFFFFF)),),
-            )
-
-            
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+        
+        
+              SizedBox(height: 1.23.h),
+              Text('Complete your Profile',
+              style: GoogleFonts.poppins(
+                fontSize: 16.66.sp,
+                fontWeight:FontWeight.w600,
+                color: Color(0xFF25054D)
+                ),
+                ),
+                 SizedBox(height: 3.93.h),
+        
+             
+              _textfield('User name'),
+        
+        
+              _textbox('Job type'),
+              
+        
+              _textbox('Total years of experience'),
+        
+             
+              _textbox('Job category'),
+        
+             
+              _textbox('Current job time'),
+        
+             
+              _textfield('Resume link'),
+              
+              
+              SizedBox(height: 4.49.h),
+              Container(
+                alignment: Alignment.center,
+                height: 5.50.h,
+                width: 85.40.w,
+                decoration: BoxDecoration(
+                  color: Color(0xFF1F41BA),
+                  borderRadius:BorderRadius.circular(5.61.h),
+                ),
+                child: Text('Submit your profile',
+                style: GoogleFonts.poppins(
+                  fontSize: 11.66.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFFFFFFFF)
+                ),
+                ),
+              )
+         
+            ],
+          ),
         ),
       ),
     );
@@ -86,36 +91,70 @@ class UserDetailScreen extends StatelessWidget {
 }
 
 
- textfield() {
-    return Container(
-          height: 4.94.h,
-          width: 84.78.w,
-          decoration: BoxDecoration(
-            border: Border.all(color: Color(0xFFCBCBCB)),
-            borderRadius: BorderRadius.circular(15),
+ _textfield(String text) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+         Text(text,
+            style: GoogleFonts.poppins(
+              fontSize: 10.sp,
+              fontWeight: FontWeight.w600
             ),
-          child:TextField(
-            decoration:InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(15)
             ),
-          ),
-        ); 
+            SizedBox(height: 0.89.h),
+
+        Container(
+          alignment: Alignment.centerLeft,
+              height: 4.94.h,
+              width: 84.78.w,
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFFCBCBCB)),
+                borderRadius: BorderRadius.circular(15),
+                ),
+
+              child:TextFormField(
+                decoration:InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.fromLTRB(2.89.w, 1.34.h, 2.89.w, 2.1.h)
+                ),
+                style: GoogleFonts.poppins(
+                  color: Color(0xFF1F41BA),
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.w600
+                ),
+
+
+              ),
+            ),
+             SizedBox(height: 1.68.h),
+      ],
+    ); 
   }
 
 
- textbox() {
-    return Container(
-          alignment: Alignment.centerRight,
-          height: 4.94.h,
-          width: 84.78.w,
-          decoration: BoxDecoration(
-            border: Border.all(color: Color(0xFFCBCBCB)),
-            borderRadius: BorderRadius.circular(15),
+ _textbox(String text) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+         Text(text,
+            style: GoogleFonts.poppins(
+              fontSize: 10.sp,
+              fontWeight: FontWeight.w600
             ),
-          child: InkWell(
-            onTap: (){},
-            child: Icon(Icons.arrow_drop_down),
-          ),
-          );
+            ),
+            SizedBox(height: 0.89.h),
+        Container(
+              alignment: Alignment.centerRight,
+              height: 4.94.h,
+              width: 84.78.w,
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFFCBCBCB)),
+                borderRadius: BorderRadius.circular(15),
+                ),
+              child: DropdownButton(
+                items:null, onChanged: null)
+              ),
+               SizedBox(height: 1.68.h),
+      ],
+    );
   }
