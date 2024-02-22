98070dd6c0d2ffa598ccaa7930db5b4393f2fa70
muhammed-fayaz <99132360+muhammed-fayaz@users.noreply.github.com>
2024-02-22 20:51:32 +0530

    hlo

--- lib/Screens/HomeScreen/Component/HomeViewScreen.dart
+++ lib/Screens/HomeScreen/Component/HomeViewScreen.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
 class HomeViewScreen extends StatelessWidget {
   const HomeViewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    color: Color(0xFF10054D),
-    child: 
-    Column(
-      children: [
-        Text("FIND YOUR DREAM JOB",
-         
-               ),
+    return Material(
+      child: Container(
         
-      ],
+      color: Color.fromARGB(255, 22, 9, 97),
+      child: 
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 70,),
+          Image.asset("assets/pic.png"),
+          SizedBox(height: 30,),
+          Text("Find Your Dream\n              Job",
+          style: GoogleFonts.poppins(
+                 fontSize: 32,
+                 fontWeight: FontWeight.w700,
+                 color: Color(0xFFFEFEFE),
+
+                 
+                 
+                  
+                 
       
-    ),
+          ),
+          
+                  
+                 ),
+                SizedBox(height: 20,),
+                 Text("Swipe through thousands of job listings in \n   the UAE from top companies. Discover \nnew opportunities that match your skills \n                     and experience",
+                 style: GoogleFonts.nunitoSans(
+                 fontSize: 16,
+                 fontWeight: FontWeight.w600,
+                 color: Color(0xFF7B88B3),
+
+                 
+                 
+                  
+                 
+      
+          ),
+                 )
+          
+        ],
+        
+      ),
+      ),
     );
   }
 }
