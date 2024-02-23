fa36d6b88ddbc435f7f03259a0f84eaa3162b976
ReemaShareen <99016463+Reemasalam@users.noreply.github.com>
2024-02-24 00:41:18 +0530

    onvboard crcted

--- lib/Screens/Onboarding/components/OnboardCard.dart
+++ lib/Screens/Onboarding/components/OnboardCard.dart
@@ -7,46 +7,50 @@ class OnboardCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(child: Container(
-          height: 58.76.h,
-          width: 113.28.w,
-          color:Color.fromRGBO(28, 37, 93, 1),margin: EdgeInsets.all(40),
-          child: Center(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(51.68),
-                  child: Container(
-                    //height: 3.247.h,
-                   //width: 29.29.w,
-                    child: Text("Free Trial",
-                    style: GoogleFonts.poppins
-                    (fontSize: 24,fontWeight: FontWeight.w700 ,
-                    color:Color.fromRGBO(255, 255, 255, 1)
-)),),
+    return
+      Column(
+        children: [
+          Container(child: Container(
+            height: 58.76.h,
+            width: 113.28.w,
+            color:Color.fromRGBO(28, 37, 93, 1),margin: EdgeInsets.all(40),
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 7.07.h,),
+            
+                     Container(
+                      //height: 3.247.h,
+                     //width: 29.29.w,
+                      child: Text("Free Trial",
+                      style: GoogleFonts.poppins
+                      (fontSize: 24,fontWeight: FontWeight.w700 ,
+                      color:Color.fromRGBO(255, 255, 255, 1)
+      )),),
+                    
+                SizedBox(height:5.05.h),
+                  SizedBox(
+                    //height: 2.24..h,
+                  //width: 25.36.w,
+                  child: Text("\$0.00",style: GoogleFonts.poppins(fontSize: 30.sp, 
+                   fontWeight: FontWeight.w700 ,
+                      color:Color.fromRGBO(255, 255, 255, 1),),),
                   ),
-                SizedBox(height: 40.930.h,
-                width: 25.36.w,
-                child: Text("\$0.00",style: GoogleFonts.poppins(fontSize: 33, 
-                 fontWeight: FontWeight.w700 ,
-                    color:Color.fromRGBO(255, 255, 255, 1))),
-                ),
-                
-                SizedBox(
-                  height: 30.85.h,
-                width: 20.11.w,
-                child: Text("For 2 days",style: GoogleFonts.poppins(fontSize: 15, 
-                 fontWeight: FontWeight.w400 ,
-                    color:Color.fromRGBO(255, 255, 255, 1))),
-                )
-              ],
+                  SizedBox(height:5.28.h),
+                  SizedBox(
+                   // height: 30.85.h,
+                  //width: 20.11.w,
+                  child: Text("For 2 days",style: GoogleFonts.poppins(fontSize: 15, 
+                   fontWeight: FontWeight.w400 ,
+                      color:Color.fromRGBO(255, 255, 255, 1))),
+                  )
+                ],
+              ),
             ),
+      ),
           ),
-),
-        ),
-      ],
-    ); 
+        ],
+      );
+    
   }
 } 
