a946b80a579420ee99d7907dd63d50316ad7d812
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-02-23 23:13:34 +0530

    fix

--- lib/Screens/Views/ViewScreen.dart
+++ lib/Screens/Views/ViewScreen.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:jobjet/Screens/Views/components/Searchbar.dart';
-
 
 class ViewScreen extends StatelessWidget {
   const ViewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SearchScreen()
-           
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
