a04f1aa72bb1d3c50a0a194fbcf3dab80b9cae3c
Neynu u <nayanaraj578@gmail.com>
2024-02-24 15:44:39 +0530

    hlloiii

--- lib/Screens/Onboarding/components/OnboardCard.dart
+++ lib/Screens/Onboarding/components/OnboardCard.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:jobjet/main.dart';
 import 'package:sizer/sizer.dart';
 
 class OnboardCardScreen extends StatelessWidget {
@@ -7,50 +10,84 @@ class OnboardCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Column(
-        children: [
-          Container(child: Container(
+    return Column(
+      children: [
+        Container(
+          child: Container(
             height: 58.76.h,
             width: 113.28.w,
-            color:Color.fromRGBO(28, 37, 93, 1),margin: EdgeInsets.all(40),
+            color: Color.fromRGBO(28, 37, 93, 1),
+            margin: EdgeInsets.all(40),
             child: Center(
               child: Column(
                 children: [
-                  SizedBox(height: 7.07.h,),
-            
-                     Container(
-                      //height: 3.247.h,
-                     //width: 29.29.w,
-                      child: Text("Free Trial",
-                      style: GoogleFonts.poppins
-                      (fontSize: 24,fontWeight: FontWeight.w700 ,
-                      color:Color.fromRGBO(255, 255, 255, 1)
-      )),),
-                    
-                SizedBox(height:5.05.h),
+                  SizedBox(
+                    height: 7.07.h,
+                  ),
+                  Container(
+                    //height: 3.247.h,
+                    //width: 29.29.w,
+                    child: Text("Free Trial",
+                        style: GoogleFonts.poppins(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: Color.fromRGBO(255, 255, 255, 1))),
+                  ),
+                  SizedBox(height: 5.05.h),
                   SizedBox(
                     //height: 2.24..h,
-                  //width: 25.36.w,
-                  child: Text("\$0.00",style: GoogleFonts.poppins(fontSize: 30.sp, 
-                   fontWeight: FontWeight.w700 ,
-                      color:Color.fromRGBO(255, 255, 255, 1),),),
+                    //width: 25.36.w,
+                    child: Text(
+                      "\$0.00",
+                      style: GoogleFonts.poppins(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                      ),
+                    ),
                   ),
-                  SizedBox(height:5.28.h),
+                  SizedBox(height: 5.28.h),
                   SizedBox(
-                   // height: 30.85.h,
-                  //width: 20.11.w,
-                  child: Text("For 2 days",style: GoogleFonts.poppins(fontSize: 15, 
-                   fontWeight: FontWeight.w400 ,
-                      color:Color.fromRGBO(255, 255, 255, 1))),
-                  )
+                    // height: 30.85.h,
+                    //width: 20.11.w,
+                    child: Text("For 2 days",
+                        style: GoogleFonts.poppins(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(255, 255, 255, 1))),
+                  ),
+                  SizedBox(height: 5.28.h),
+                  SizedBox(
+                    // height: 30.85.h,
+                    width: 59.66.w,
+                    child: Text(
+                        "Experience First, Decide Later - No Credit Card Required. Enjoy Unlimited Job Views in the UAE for 2 Days with Free Access to All App Features.",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.nunitoSans(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromRGBO(255, 255, 255, 1))),
+                  ),
+                  SizedBox(
+                    height: 5.07.h,
+                  ),
+                  Container(
+                    color: Color.fromRGBO(31, 65, 186, 1),
+                    // height: 5.84.h, width: 62.31.w,
+
+                    child: Text("Start free Trial",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromRGBO(255, 255, 255, 1))),
+                  ),
                 ],
               ),
             ),
-      ),
           ),
-        ],
-      );
-    
+        ),
+      ],
+    );
   }
-} 
+}
