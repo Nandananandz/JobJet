253904c23a756131f6343969ec37b7891db28882
Neynu u <nayanaraj578@gmail.com>
2024-02-24 18:03:29 +0530

    finish

--- lib/Screens/Onboarding/components/OnboardCard.dart
+++ lib/Screens/Onboarding/components/OnboardCard.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+//import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,13 +11,18 @@ class OnboardCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
+      children: [SizedBox(height: 23.14.h),
         Container(
           child: Container(
             height: 58.76.h,
             width: 113.28.w,
-            color: Color.fromRGBO(28, 37, 93, 1),
+            //color: Color.fromRGBO(28, 37, 93, 1),
             margin: EdgeInsets.all(40),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(17),
+              color: Color.fromRGBO(28, 37, 93, 1),
+
+            ),
             child: Center(
               child: Column(
                 children: [
@@ -52,35 +57,41 @@ class OnboardCardScreen extends StatelessWidget {
                     //width: 20.11.w,
                     child: Text("For 2 days",
                         style: GoogleFonts.poppins(
-                            fontSize: 15,
+                            fontSize: 12.5.sp,
                             fontWeight: FontWeight.w400,
                             color: Color.fromRGBO(255, 255, 255, 1))),
                   ),
-                  SizedBox(height: 5.28.h),
+                  SizedBox(height: 3.70.h),
                   SizedBox(
-                    // height: 30.85.h,
-                    width: 59.66.w,
+                    // height: 5.61.h,
+                    width: 60.24.w,
                     child: Text(
                         "Experience First, Decide Later - No Credit Card Required. Enjoy Unlimited Job Views in the UAE for 2 Days with Free Access to All App Features.",
                         textAlign: TextAlign.center,
                         style: GoogleFonts.nunitoSans(
-                            fontSize: 16,
+                            fontSize: 13.33.sp,
                             fontWeight: FontWeight.w600,
                             color: Color.fromRGBO(255, 255, 255, 1))),
                   ),
                   SizedBox(
                     height: 5.07.h,
                   ),
-                  Container(
-                    color: Color.fromRGBO(31, 65, 186, 1),
-                    // height: 5.84.h, width: 62.31.w,
+                  //InkWell(onTap: (){get.to back },
+                 Container(decoration: BoxDecoration(color: Color.fromRGBO(31, 65, 186, 1),
+                  borderRadius: BorderRadius.circular(50 ),),
+                   // color: Color.fromRGBO(31, 65, 186, 1),
+                     height: 5.14.h,
+                     width: 69.31.w,
 
-                    child: Text("Start free Trial",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Color.fromRGBO(255, 255, 255, 1))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Start free Trial",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                              fontSize: 13.33.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromRGBO(255, 255, 255, 1))),
+                    ),
                   ),
                 ],
               ),
